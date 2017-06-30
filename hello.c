e5afc9aa8b04203a5336f9499a09387b30724709
shimonibarak <barak_shimoni@hotmail.com>
2017-06-29 17:06:22 -0700

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,4 +1,8 @@
-/* hello soucre file */
+/* hello soucre file (BETA only)  */
 /* revision 1 - add 1st line */
 /* revision 1 - add 2nd line */
+
+/***********************************/
+/********* for BETA only ***********/
+/***********************************/
 /* EOF */
